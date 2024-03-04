34b35501d384f72958bfd2b88f93258694628aea
KristianBarzilov06 <kikogaming0@gmail.com>
2024-03-04 19:26:41 +0200

    Tolkoz ot men

--- PetClinic/Controller/ClientController.cs
+++ PetClinic/Controller/ClientController.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace PetClinic.Controller
 {
-    class MainController
+    class ClientController
     {
         public List<Client> GetAllClients()
         {
@@ -49,50 +49,6 @@ namespace PetClinic.Controller
                 if(clientToDelete != null)
                 {
                     db.Clients.Remove(clientToDelete);
-                    db.SaveChanges();
-                }
-            }
-        }
-        public List<Doctor> GetAllDoctors()
-        {
-            using (PetClinicDBEntities db = new PetClinicDBEntities())
-            {
-                return db.Doctors.ToList();
-            }
-        }
-
-        public void AddDoctor(Doctor doctor)
-        {
-            using (PetClinicDBEntities db = new PetClinicDBEntities())
-            {
-                doctor.Id = db.Doctors.ToList().LastOrDefault().Id + 1;
-                db.Doctors.Add(doctor);
-                db.SaveChanges();
-            }
-        }
-
-        public void UpdateDoctor(Doctor doctor)
-        {
-            using (PetClinicDBEntities db = new PetClinicDBEntities())
-            {
-                var doctorToUpdate = db.Doctors.Where(d => d.Id == doctor.Id).FirstOrDefault();
-                if (doctorToUpdate != null)
-                {
-                    doctorToUpdate.Id = doctor.Id;
-                    doctorToUpdate.Username = doctor.Username;
-                    doctorToUpdate.Password = doctor.Password;
-                    db.SaveChanges();
-                }
-            }
-        }
-        public void DeleteClient(Doctor doctor)
-        {
-            using (PetClinicDBEntities db = new PetClinicDBEntities())
-            {
-                var doctorToDelete = db.Doctors.Where(d => d.Id == doctor.Id).FirstOrDefault();
-                if (doctorToDelete != null)
-                {
-                    db.Doctors.Remove(doctorToDelete);
                     db.SaveChanges();
                 }
             }
