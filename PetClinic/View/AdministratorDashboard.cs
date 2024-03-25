aeb3875900b5b79848bfd56b166844ae27da3d17
Мариян Савев <ms72233888@edu.mon.bg>
2024-03-25 20:11:11 +0200

    Add Administrator Panel

---
+++ PetClinic/View/AdministratorDashboard.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PetClinic.View
+{
+    public partial class AdministratorDashboard : Form
+    {
+        public AdministratorDashboard()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void AdministratorDashboard_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void UsernameLb_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
